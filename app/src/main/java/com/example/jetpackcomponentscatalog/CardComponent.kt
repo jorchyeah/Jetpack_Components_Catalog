03b10e9a1ffea3f2280a7d3f0b5a16188c65aa5f
jorchyeah <jc.3@live.com.mx>
2023-06-26 14:00:16 -0600

    Added card components

---
+++ app/src/main/java/com/example/jetpackcomponentscatalog/CardComponent.kt
@@ -0,0 +1,18 @@
+package com.example.jetpackcomponentscatalog
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MyCard() {
+    Card(
+        Modifier.padding(32.dp), elevation = 12.dp
+    ) {
+        Text(
+            text = "Hola")
+    }
+}
