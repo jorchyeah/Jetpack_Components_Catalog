4820a61e1523ba4e1a7b89e6e495850560e1a2b4
jorchyeah <jc.3@live.com.mx>
2023-06-27 17:54:17 -0600

    Added the header for the login screen

--- app/src/main/java/com/example/jetpackcomponentscatalog/LoginScreen.kt
+++ app/src/main/java/com/example/jetpackcomponentscatalog/LoginScreen.kt
@@ -1,6 +1,34 @@
 package com.example.jetpackcomponentscatalog
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoginScreen() {}
+fun LoginScreen() {
+    //val activity = LocalContext.current as Activity
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
+        Header(
+            Modifier
+                .align(Alignment.TopEnd)
+            //.clickable { activity.finish() }
+        )
+    }
+}
+
+@Composable
+fun Header(modifier: Modifier) {
+    Icon(imageVector = Icons.Default.Close, contentDescription = "Close App", modifier = modifier)
+}
