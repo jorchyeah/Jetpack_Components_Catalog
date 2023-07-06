c85ab73335ba8eeaa487c2d4ff9767abf37b09a8
jorchyeah <jc.3@live.com.mx>
2023-07-06 14:10:51 -0600

    Added buttons to ButtonComponent.kt

--- app/src/main/java/com/example/jetpackcomponentscatalog/ButtonComponent.kt
+++ app/src/main/java/com/example/jetpackcomponentscatalog/ButtonComponent.kt
@@ -1,12 +1,10 @@
 package com.example.jetpackcomponentscatalog
 
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,4 +26,39 @@ fun MyButton() {
             Text(text = "Hola Mundo")
         }
     }
+}
+
+@Composable
+fun MyLikeButton() {
+    Button(
+        onClick = { },
+        contentPadding = PaddingValues(
+            start = 20.dp,
+            top = 12.dp,
+            end = 20.dp,
+            bottom = 12.dp
+        )
+    ) {
+        Icon(
+            Icons.Filled.Favorite,
+            contentDescription = "Favorite",
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+        Text("Like")
+    }
+}
+
+@Composable
+fun MyExtendedLikeButton() {
+    ExtendedFloatingActionButton(
+        onClick = { /* ... */ },
+        icon = {
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Favorite"
+            )
+        },
+        text = { Text("Like") }
+    )
 }
