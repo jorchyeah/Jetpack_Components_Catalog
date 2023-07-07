9b8d3aedccd4e3320d03153320c35bd29a6451b0
jorchyeah <jc.3@live.com.mx>
2023-07-06 18:38:07 -0600

    Added a model package for the recycler view component screen

---
+++ app/src/main/java/com/example/jetpackcomponentscatalog/model/PokemonTypes.kt
@@ -0,0 +1,22 @@
+package com.example.jetpackcomponentscatalog.model
+
+enum class PokemonTypes (val color: Long) {
+    NORMAL(0xFFA8A878),
+    FIRE(0xFFF08030),
+    WATER(0xFF6890F0),
+    GRASS(0xFF78C850),
+    ELECTRIC(0xFFF8D030),
+    ICE(0xFF98D8D8),
+    FIGHTING(0xFFC03028),
+    POISON(0xFFA040A0),
+    GROUND(0xFFE0C068),
+    FLYING(0xFFA890F0),
+    PSYCHIC(0xFFF85888),
+    BUG(0xFFA8B820),
+    ROCK(0xFFB8A038),
+    GHOST(0xFF705898),
+    DRAGON(0xFF7038F8),
+    DARK(0xFF705848),
+    STEEL(0xFFB8B8D0),
+    FAIRY(0xFFEE99AC)
+}
